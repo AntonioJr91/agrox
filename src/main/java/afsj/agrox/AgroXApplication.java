670e62b2c9920c29a91b4cfa8e5afebfe905eb5f
AntonioJr <afs.junior21@gmail.com>
2025-12-12 17:31:01 -0300

    first commit

---
+++ src/main/java/afsj/agrox/AgroXApplication.java
@@ -0,0 +1,13 @@
+package afsj.agrox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgroXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/afsj/agrox/AgroXApplicationTests.java
@@ -0,0 +1,13 @@
+package afsj.agrox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
