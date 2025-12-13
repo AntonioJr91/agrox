d3cdb071bf6ba9eabf81a5a3b9963920db0c683f
AntonioJr <afs.junior21@gmail.com>
2025-12-13 18:13:46 -0300

    feat: add category entity

---
+++ src/main/java/afsj/agrox/entities/Category.java
@@ -0,0 +1,64 @@
+package afsj.agrox.entities;
+
+import afsj.agrox.validations.DomainValidation;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Category {
+   private Long id;
+   private String name;
+
+   private Set<Product> products = new HashSet<>();
+
+   protected Category() {
+   }
+
+   public Category(String name) {
+      validateName(name);
+      this.name = name;
+   }
+
+   public Long getId() {
+      return id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      validateName(name);
+      this.name = name;
+   }
+
+   public Set<Product> getProducts() {
+      return Set.copyOf(products);
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (!(o instanceof Category category)) return false;
+      return id != null && id.equals(category.id);
+   }
+
+   @Override
+   public int hashCode() {
+      return 31;
+   }
+
+   @Override
+   public String toString() {
+      return "Category{" +
+              "id=" + id +
+              ", name='" + name + '\'' +
+              '}';
+   }
+
+   private void validateName(String name) {
+      DomainValidation.when(name == null, "Category name is required");
+      DomainValidation.when(name.isBlank(), "Category name must not be blank");
+      DomainValidation.when(name.length() < 3 || name.length() > 50,
+              "Category name must contain between 3 and 50 characters");
+   }
+}
