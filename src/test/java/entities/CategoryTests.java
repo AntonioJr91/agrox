076ea5d8c9150c0bbf4e0823abbe51d33d78f643
AntonioJr <afs.junior21@gmail.com>
2025-12-13 23:18:55 -0300

    tests: add unit tests for category

---
+++ src/test/java/entities/CategoryTests.java
@@ -0,0 +1,46 @@
+package entities;
+
+import afsj.agrox.entities.Category;
+import afsj.agrox.exceptions.DomainException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CategoryTests {
+   Category category;
+   private String validName;
+
+   @BeforeEach
+   void setUp() {
+      validName = "Food";
+
+      category = new Category(validName);
+   }
+
+   @Test
+   void shouldCreateWhenIsValid() {
+      Assertions.assertEquals(validName, category.getName());
+   }
+
+   @Test
+   void shouldThrowExceptionWhenNameIsNull() {
+      Assertions.assertThrows(
+              DomainException.class, () -> new Category(null)
+      );
+   }
+
+   @Test
+   void shouldThrowExceptionWhenNameHasLessThan3Characters() {
+      Assertions.assertThrows(
+              DomainException.class,
+              () -> new Category("a".repeat(2))
+      );
+   }
+   @Test
+   void shouldThrowExceptionWhenNameHasMoreThan50Characters() {
+      Assertions.assertThrows(
+              DomainException.class,
+              () -> new Category("a".repeat(51))
+      );
+   }
+}
