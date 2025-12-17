effccdc7a2bd364424df222f5b5bc5d12b6dbeaf
AntonioJr <afs.junior21@gmail.com>
2025-12-17 13:35:58 -0300

    feat: add custom error handling ResourceNotFound

--- src/main/java/afsj/agrox/services/CategoryService.java
+++ src/main/java/afsj/agrox/services/CategoryService.java
@@ -3,6 +3,7 @@ package afsj.agrox.services;
 import afsj.agrox.dtos.CategoryCreateDto;
 import afsj.agrox.dtos.CategoryResponseDto;
 import afsj.agrox.entities.Category;
+import afsj.agrox.exceptions.ResourceNotFoundException;
 import afsj.agrox.mapper.CategoryMapper;
 import afsj.agrox.repositories.CategoryRepository;
 import org.springframework.stereotype.Service;
@@ -26,7 +27,7 @@ public class CategoryService {
 
    @Transactional(readOnly = true)
    public CategoryResponseDto findCategoryById(Long id) {
-      Category category = categoryRepository.findById(id).orElseThrow(() -> new IllegalArgumentException());
+      Category category = categoryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException());
       return CategoryMapper.toDto(category);
    }
 
@@ -40,7 +41,7 @@ public class CategoryService {
    @Transactional
    public void deleteCategory(Long id) {
       if (!categoryRepository.existsById(id)) {
-         throw new IllegalArgumentException();
+         throw new ResourceNotFoundException();
       }
       categoryRepository.deleteById(id);
    }
